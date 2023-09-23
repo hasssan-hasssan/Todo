c3435a7d2a4c92f62a7efce416120470d16e046c
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2023-09-23 23:14:35 +0330

    Created 'Task' model and register in admin panel and update the database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    """ Tasks model """
+    text = models.CharField(max_length=250)
+    taskDone = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # Each user has own tasks
+    uiSettings = models.JSONField(null=True, blank=True) # Each user can customize own UI, for example : color, font , radius and etc
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.text[:100]

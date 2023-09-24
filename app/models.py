a6a9f743160ae7f295f0b1096b04997469d08f05
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2023-09-24 06:05:21 +0330

    Remove uiSetting field from task model and convert it to another model

--- app/models.py
+++ app/models.py
@@ -7,9 +7,13 @@ class Task(models.Model):
     text = models.CharField(max_length=250)
     taskDone = models.BooleanField(default=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE) # Each user has own tasks
-    uiSettings = models.JSONField(null=True, blank=True) # Each user can customize own UI, for example : color, font , radius and etc
     create = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.text[:100]
+
+
+class UiSetting(models.Model):
+    uiSettings = models.JSONField(null=True, blank=True) # Each user can customize own UI, for example : color, font , radius and etc
+    author = models.OneToOneField(User, on_delete=models.CASCADE)

a681e609516f3ff0dab2107f46971b5b9266b041
Abolfazl <abolfazl.hassanzade.81@gmail.com>
2023-09-24 06:06:38 +0330

    fetch todo list and show

--- app/views.py
+++ app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+def todoList(request):
+    user = request.user
+    todo_list = user.task_set.all()
+    return render(request, 'list.html', {'todo_list':todo_list})
